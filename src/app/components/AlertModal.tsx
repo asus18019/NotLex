53b5a0f161875c96828c08225393cfaf36ce7071
Viktor Volokhan <voloxanvitya@gmail.com>
2023-08-30 16:42:01 +0300

    add alert modal

---
+++ src/app/components/AlertModal.tsx
@@ -0,0 +1,53 @@
+import { Alert, Box, styled, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { ModalDataType } from '@/types';
+import { AlertTimeout } from '@/config/AlertTimeout';
+
+const AlertWindow = styled(Alert)(({ theme }) => ({
+	cursor: 'pointer',
+	padding: '4px 16px',
+	position: 'fixed',
+	top: 30,
+	left: 10,
+	right: 10,
+	bottom: 'auto',
+	zIndex: 999,
+	[theme.breakpoints.up('md')]: {
+		top: 'auto',
+		left: 'auto',
+		right: 30,
+		bottom: 30
+	}
+}));
+
+
+interface AlertModalProps {
+	modalData: ModalDataType;
+	handleClickModal: () => void;
+}
+
+const AlertModal = ({ modalData, handleClickModal }: AlertModalProps) => {
+	const [lineWidth, setLineWidth] = useState('100%');
+	console.log(lineWidth);
+
+	useEffect(() => {
+		setLineWidth('0%');
+
+	}, [modalData.message]);
+
+	return (
+		<AlertWindow onClick={ handleClickModal } severity={ modalData.type }>
+			<Box
+				height="2px"
+				bgcolor="blue"
+				sx={ {
+					width: lineWidth,
+					transitionDuration: `${ AlertTimeout / 1000 }s`
+				} }
+			/>
+			<Typography fontWeight={ 500 }>{ modalData.message }</Typography>
+		</AlertWindow>
+	);
+};
+
+export default AlertModal;
