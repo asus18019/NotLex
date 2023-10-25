ba1edf45dab3d965fc8766b16655745ad2f111f5
Viktor Volokhan <voloxanvitya@gmail.com>
2023-10-25 10:28:04 +0300

    add middleware chain

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,50 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextFetchEvent, NextMiddleware, NextRequest, NextResponse } from 'next/server';
 import { checkSecrets } from '@/utils/checkCredentials';
 
-export async function middleware(req: NextRequest) {
-	const credentials = req.cookies.get('credentials');
-	const { secret, database_id } = JSON.parse(credentials?.value || '{}');
+const privateRoutes = ['/dashboard', '/add', '/settings'];
 
-	if(!secret || !database_id) {
-		return NextResponse.redirect(new URL('/', req.url))
-	}
+export default withMiddleware1();
 
-	try {
-		const res = await checkSecrets({ secret, database_id });
-		if(!res.ok) {
-			new Error('This route is unavailable');
+const runNextMiddleware = (req: NextRequest, event: NextFetchEvent, middleware?: NextMiddleware) => {
+	return middleware && middleware(req, event);
+};
+
+const matchRoute = (currentRoute: string, routes: string[]): boolean => {
+	let isMatchRoute = false;
+	routes.forEach(route => {
+		if(currentRoute.includes(route)) {
+			isMatchRoute = true;
 		}
-	} catch(error) {
-		console.log(error);
-		return NextResponse.redirect(new URL('/', req.url))
-	}
-}
+	});
+	return isMatchRoute;
+};
 
+function withMiddleware1(middleware?: NextMiddleware) {
+	return async (req: NextRequest, event: NextFetchEvent) => {
+		if(!matchRoute(req.url, privateRoutes)) {
+			return runNextMiddleware(req, event, middleware);
+		}
+
+		const credentials = req.cookies.get('credentials');
+		const { secret, database_id } = JSON.parse(credentials?.value || '{}');
+
+		if(!secret || !database_id) {
+			return NextResponse.redirect(new URL('/', req.url));
+		}
+
+		try {
+			const res = await checkSecrets({ secret, database_id });
+			if(!res.ok) {
+				new Error('This route is unavailable');
+			}
+		} catch(error) {
+			console.log(error);
+			return NextResponse.redirect(new URL('/', req.url));
+		}
+		return runNextMiddleware(req, event, middleware);
+	};
+}
 
 export const config = {
-	matcher: ['/dashboard', '/add', '/settings']
-}
+	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+};
