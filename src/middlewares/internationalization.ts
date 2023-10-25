671eef0f702a0c9356a7477c5b45db574f9e6c81
Viktor Volokhan <voloxanvitya@gmail.com>
2023-10-25 14:45:26 +0300

    fix images (now it ignores the locale if an image is requested)

--- src/middlewares/internationalization.ts
+++ src/middlewares/internationalization.ts
@@ -1,21 +1,30 @@
 import { NextFetchEvent, NextMiddleware, NextRequest, NextResponse } from 'next/server';
 import { runNextMiddleware } from '@/middleware';
 import Negotiator from 'negotiator';
-import { match as matchLocale } from '@formatjs/intl-localematcher'
+import { match as matchLocale } from '@formatjs/intl-localematcher';
 import { i18n } from '../../i18n.config';
+
+const allowedImageExtension = ['.svg', '.png'];
 
 export function withMiddleware2(middleware?: NextMiddleware) {
 	return async (req: NextRequest, event: NextFetchEvent) => {
-		const pathname = req.nextUrl.pathname
-		const pathnameIsMissingLocale = i18n.locales.every(
-			locale => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-		)
+		const pathname = req.nextUrl.pathname;
 
-		if (pathnameIsMissingLocale) {
-			const locale = getLocale(req)
+		for(const extension of allowedImageExtension) {
+			if(pathname.endsWith(extension)) {
+				return runNextMiddleware(req, event, middleware);
+			}
+		}
+
+		const pathnameIsMissingLocale = i18n.locales.every(
+			locale => !pathname.startsWith(`/${ locale }/`) && pathname !== `/${ locale }`
+		);
+
+		if(pathnameIsMissingLocale) {
+			const locale = getLocale(req);
 			return NextResponse.redirect(
-				new URL(`/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`, req.url)
-			)
+				new URL(`/${ locale }${ pathname.startsWith('/') ? '' : '/' }${ pathname }`, req.url)
+			);
 		}
 
 		return runNextMiddleware(req, event, middleware);
@@ -23,12 +32,12 @@ export function withMiddleware2(middleware?: NextMiddleware) {
 }
 
 function getLocale(request: NextRequest): string | undefined {
-	const negotiatorHeaders: Record<string, string> = {}
-	request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
+	const negotiatorHeaders: Record<string, string> = {};
+	request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
 
 	// @ts-ignore locales are readonly
-	const locales: string[] = i18n.locales
-	const languages = new Negotiator({ headers: negotiatorHeaders }).languages()
+	const locales: string[] = i18n.locales;
+	const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
 
 	return matchLocale(languages, locales, i18n.defaultLocale);
 }
